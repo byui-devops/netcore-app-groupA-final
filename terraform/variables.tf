c178ba3abb78f1cb3abb16bd8a4bcef27d72353b
Ethan Trent <130587485+ethantrent@users.noreply.github.com>
2026-04-06 20:33:10 -0600

    add # terraform variables

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "docker_image" {
+  description = "Docker image to pull and run on the EC2 instance"
+  type        = string
+}
